76c725b18c855164c2e7a115c040bacfc0588561
katerinantn <katerinantoniades@gmail.com>
2024-05-24 18:41:46 +0300

    Create create_users.sql

---
+++ create_users.sql
@@ -0,0 +1,19 @@
+-- CREATE USERS AND GRANT ACCESS --
+-- ---------------------------------
+
+-- CREATE USERS --
+
+-- SIMPLE COOK USER --
+CREATE USER 'cook_user'@'localhost' IDENTIFIED BY 'cook_password';
+
+-- Grant SELECT permissions on CookPersonalInfo view to the user
+GRANT SELECT,UPDATE ON Cook_Personal_Info TO 'cook_user'@'localhost';
+-- Grant SELECT permissions on CookRecipes view to the user
+GRANT SELECT,UPDATE ON Cook_Recipes TO 'cook_user'@'localhost';
+
+
+-- ADMIN USER --
+CREATE USER 'admin_user'@'localhost' IDENTIFIED BY 'admin_password';
+
+--GRANT ALL permissions on the database to the user
+GRANT ALL PRIVILEGES ON masterchef.* TO 'admin_user'@'localhost';
